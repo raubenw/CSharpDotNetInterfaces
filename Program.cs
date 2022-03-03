a2283a569aa3a8757c95bab65cf01fbf8cb91f88
raubenw <raubenw@gmail.com>
2022-03-03 14:21:28 +0200

    Eerste commit

---
+++ Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+
+namespace CSharpDotNetInterfaces
+{
+
+    interface IStorable
+    {
+        void Load();
+        void Save();
+        Boolean NeedsSave { get; set; }
+    }
+
+    class Document : IStorable, INotifyPropertyChanged
+    {
+
+        private string _title;
+        private Boolean mNeedsSave = false;
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public Document(string aTitle)
+        {
+            _title = aTitle;
+            Console.WriteLine("Creating new document with title '{0}'", _title);
+        }
+
+        public void Load()
+        {
+            Console.WriteLine("Loading the document");
+        }
+
+        public void Save()
+        {
+            Console.WriteLine("Saving the document");
+        }
+
+        public String DocName
+        {
+            get
+            {
+                return _title;
+            }
+
+            set
+            {
+                _title = value;
+                NotifiyPropChanged("DocName");
+            }
+        }
+
+        public Boolean NeedsSave
+        {
+            get
+            {
+                return mNeedsSave;
+            }
+
+            set
+            {
+                mNeedsSave = value;
+                NotifiyPropChanged("NeedsSave");
+            }
+        }
+
+        private void NotifiyPropChanged(string propName)
+        {
+            PropertyChanged(this, new PropertyChangedEventArgs(propName));
+        }
+    }
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            Document doc = new Document("Document New title");
+
+            doc.PropertyChanged += delegate (object sender, PropertyChangedEventArgs e)
+            {
+                Console.WriteLine("The document property {0} has changed", e.PropertyName);
+            };
+
+            doc.DocName = "My New Document Name";
+            doc.NeedsSave = true;
+
+            Console.WriteLine("Press Enter to continue...");
+            Console.ReadLine();
+        }
+    }
+}
